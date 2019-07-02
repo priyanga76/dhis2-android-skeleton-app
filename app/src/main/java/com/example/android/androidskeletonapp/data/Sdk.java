8c6453d5f9b0ccd349b3a85ea7cd94eedabee613
Priyanga Senanayaka <priyanga.senanayaka@gmail.com>
2019-07-02 12:24:37 +0200

    Solution

--- app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
@@ -8,7 +8,11 @@ public class Sdk {
 
     public static D2Configuration getD2Configuration(Context context) {
         // TODO Add configuration
-
-        return null;
+        D2Configuration configuration = D2Configuration.builder()
+                .appName("priyanga76").appVersion("1.0.0")
+                .context(context)
+                .readTimeoutInSeconds(30).connectTimeoutInSeconds(30).writeTimeoutInSeconds(30)
+                .build();
+                 return configuration;
     }
 }
