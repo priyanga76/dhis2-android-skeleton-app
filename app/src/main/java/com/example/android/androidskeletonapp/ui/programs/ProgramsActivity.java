be7a1b874a518503d27cd6305a463b10aa5104b3
Priyanga Senanayaka <priyanga.senanayaka@gmail.com>
2019-07-02 18:11:16 +0200

    completed

--- app/src/main/java/com/example/android/androidskeletonapp/ui/programs/ProgramsActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/programs/ProgramsActivity.java
@@ -7,11 +7,15 @@ import com.example.android.androidskeletonapp.R;
 import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
 
+import org.hisp.dhis.android.core.arch.helpers.UidsHelper;
+import org.hisp.dhis.android.core.organisationunit.OrganisationUnit;
 import org.hisp.dhis.android.core.program.Program;
 import org.hisp.dhis.android.core.program.ProgramType;
 
 import androidx.lifecycle.LiveData;
 import androidx.paging.PagedList;
+
+import java.util.List;
 
 public class ProgramsActivity extends ListActivity implements OnProgramSelectionListener {
 
@@ -28,7 +32,15 @@ public class ProgramsActivity extends ListActivity implements OnProgramSelection
 
         // TODO Filter and sort Programs by orgUnit and displayName
 
+        List<OrganisationUnit> orgUnits = Sdk.d2().organisationUnitModule().organisationUnits
+                .byOrganisationUnitScope(OrganisationUnit.Scope.SCOPE_DATA_CAPTURE)
+                .get();
+
+        List<String> orgUnitUids = UidsHelper.getUidsList(orgUnits);
+
+
         LiveData<PagedList<Program>> programs = Sdk.d2().programModule().programs
+                .byOrganisationUnitList(orgUnitUids)
                 .withStyle()
                 .getPaged(20);
 
