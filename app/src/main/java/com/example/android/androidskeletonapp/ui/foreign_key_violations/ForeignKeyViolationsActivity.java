ba56d867cba13c8472fb7b71184d7eab909bdb18
Priyanga Senanayaka <priyanga.senanayaka@gmail.com>
2019-07-04 15:44:41 +0200

    final commit

--- app/src/main/java/com/example/android/androidskeletonapp/ui/foreign_key_violations/ForeignKeyViolationsActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/foreign_key_violations/ForeignKeyViolationsActivity.java
@@ -1,9 +1,16 @@
 package com.example.android.androidskeletonapp.ui.foreign_key_violations;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
+
+import org.hisp.dhis.android.core.maintenance.ForeignKeyViolation;
+
+import androidx.lifecycle.LiveData;
+import androidx.paging.PagedList;
 
 public class ForeignKeyViolationsActivity extends ListActivity {
 
@@ -11,7 +18,22 @@ public class ForeignKeyViolationsActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setUp(R.layout.activity_fk_violations, R.id.fkViolationsToolbar, R.id.fkViolationsRecyclerView);
-        // TODO list foreign key violations
 
+        //TODO list foreign key violations
+        observeFKViolations();
+    }
+
+    private void observeFKViolations() {
+        ForeignKeyViolationsAdapter adapter = new ForeignKeyViolationsAdapter();
+        recyclerView.setAdapter(adapter);
+
+        LiveData<PagedList<ForeignKeyViolation>> liveData = Sdk.d2().maintenanceModule().foreignKeyViolations
+                .getPaged(20);
+
+        liveData.observe(this, fkViolationsPagedList -> {
+            adapter.submitList(fkViolationsPagedList);
+            findViewById(R.id.fkViolationsNotificator).setVisibility(
+                    fkViolationsPagedList.isEmpty() ? View.VISIBLE : View.GONE);
+        });
     }
 }
